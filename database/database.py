d8240d991c7372f68cf2f63c67416948acfca58c
Rogenowski <mateuszroggenbuk@gmail.com>
2023-11-29 00:32:43 +0100

    Added login and register

--- database/database.py
+++ database/database.py
@@ -22,23 +22,23 @@ class Product(db.Model):
         }
 
 
-class Users(db.Model):
-    __tablename__ = 'users'
+# class Users(db.Model):
+#     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    surname = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), nullable=False)
+#     surname = db.Column(db.String(80), nullable=False)
+#     email = db.Column(db.String(80), nullable=False)
+#     age = db.Column(db.Integer, nullable=False)
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'surname': self.surname,
-            'email': self.email,
-            'age': self.age
-        }
+#     def to_dict(self):
+#         return {
+#             'id': self.id,
+#             'name': self.name,
+#             'surname': self.surname,
+#             'email': self.email,
+#             'age': self.age
+#         }
 
 
 class PaymentType(db.Model):
